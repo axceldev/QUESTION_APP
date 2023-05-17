f2dfa44eb604a2d3807cdd557b0e76a4c37f2d41
axceldev <cristianbambague@unicomfacauca.edu.co>
2023-05-16 19:16:52 -0500

    Structure project init

---
+++ src/main/java/com/axceldev/questionapp/QuestionAppApplication.java
@@ -0,0 +1,13 @@
+package com.axceldev.questionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionAppApplication.class, args);
+	}
+
+}
